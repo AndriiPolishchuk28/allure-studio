4d50f5be3da29edff94b5cbbde7b0a871ef102bc
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-07-28 16:09:19 +0200

    services video

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,17 @@
+import Permanent from '@/components/Permanent/Permanent';
+import dynamic from 'next/dynamic';
+
+const ServicesVideo = dynamic(() => import('../components/Services'), {
+    ssr: false,
+});
+
 const Home = () => {
-    return <main>Home</main>;
+    return (
+        <main>
+            <Permanent />
+            <ServicesVideo />
+        </main>
+    );
 };
 
 export default Home;
---
+++ src/components/Services/ServicesItem/ServicesItem.jsx
@@ -0,0 +1,23 @@
+// import Link from 'next/link';
+import ReactPlayer from 'react-player';
+import scss from './ServicesItem.module.scss';
+import '../../../styles/globals.scss';
+
+const ServicesItem = ({ link, path, label }) => {
+    return (
+        <li className={scss.item}>
+            {/* <Link href={link}> */}
+            <ReactPlayer
+                className={scss.video_player}
+                url={path}
+                playing
+                loop
+                width={200}
+            />
+            {/* </Link> */}
+            <p className={scss.label}>{label}</p>
+        </li>
+    );
+};
+
+export default ServicesItem;
