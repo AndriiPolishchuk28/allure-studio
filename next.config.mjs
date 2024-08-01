010574751d1cb7031a6b5e9e54da69681bcbcfe2
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-08-01 16:19:19 +0200

    add next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,17 @@
-// next.config.mjs
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://player.vimeo.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; media-src 'self' https://player.vimeo.com; frame-src 'self' https://player.vimeo.com https://www.google.com;",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
-// const nextConfig = {
-//     async headers() {
-//         return [
-//             {
-//                 source: '/(.*)',
-//                 headers: [
-//                     {
-//                         key: 'Content-Security-Policy',
-//                         value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://player.vimeo.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; media-src 'self' https://player.vimeo.com; frame-src 'self' https://player.vimeo.com https://www.google.com;",
-//                     },
-//                 ],
-//             },
-//         ];
-//     },
-// };
-
-// export default nextConfig;
+export default nextConfig;
