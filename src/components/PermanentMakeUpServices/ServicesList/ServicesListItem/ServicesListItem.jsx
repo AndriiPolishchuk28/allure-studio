df0b477bee83fdd7dbe46f93836cf5bf2a266da7
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-08-03 10:52:45 +0300

    added adaptive make-up services

--- src/components/PermanentMakeUpServices/ServicesList/ServicesListItem/ServicesListItem.jsx
+++ src/components/PermanentMakeUpServices/ServicesList/ServicesListItem/ServicesListItem.jsx
@@ -5,16 +5,19 @@ import scss from './ServicesListItem.module.scss';
 const ServicesListItem = ({ id, title, image, description, onClick }) => {
     return (
         <li className={scss.item}>
-            <PageTitle style={{ textAlign: 'center' }}>{title}</PageTitle>
             <Image src={image} alt={title} className={scss.image} />
-            <p className={scss.description}>{description}</p>
-            <div className={scss.buttonWrapper}>
-                <button
-                    onClick={() => onClick(id)}
-                    className={scss.modalButton}
-                >
-                    zum service
-                </button>
+            <div className={scss.info}>
+                <PageTitle className={scss.pageTitle}>{title}</PageTitle>
+
+                <p className={scss.description}>{description}</p>
+                <div className={scss.buttonWrapper}>
+                    <button
+                        onClick={() => onClick(id)}
+                        className={scss.modalButton}
+                    >
+                        zum service
+                    </button>
+                </div>
             </div>
         </li>
     );
