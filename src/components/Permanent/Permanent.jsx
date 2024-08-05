91130b21f83a63997126e430c7f9375800cba68a
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-08-05 12:53:06 +0200

    permanent section desktop

--- src/components/Permanent/Permanent.jsx
+++ src/components/Permanent/Permanent.jsx
@@ -17,6 +17,7 @@ const Permanent = () => {
                 <p className={scss.text_owner}>by Uliana Eremenko</p>
             </div>
             <div className={scss.img_wrapper}>
+                {/* <div className={scss.test}> */}
                 <picture>
                     <source
                         media="(min-width: 1440px)"
@@ -33,6 +34,8 @@ const Permanent = () => {
                         className={scss.image}
                     />
                 </picture>
+                {/* </div> */}
+
                 <div className={scss.stick}></div>
                 <div className={scss.smear_wrapper}>
                     <div className={scss.smear_wrapper_text}>
@@ -66,6 +69,38 @@ const Permanent = () => {
                                 nun endlich unser eigenes Studio im Herzen von
                                 Berlin eröffnen. Wir sind ein Zusammenschluss
                                 aus sechs Beauty-Experten.
+                            </p>
+                        </div>
+                        <div className={scss.desktop_text_wrapper}>
+                            <p
+                                className={clsx(
+                                    scss.smear_text,
+                                    scss.desktop_text
+                                )}
+                            >
+                                2022 Inhaberin und Geschäftsführering von ALLURE
+                                Permanent Make up & Beauty, 2022 Jury-Mitgleider
+                                der German WULOP, 2021 Siegerin der German WULOP
+                                (Powder Brow), seit 2018 Permanent Make up -
+                                Artist.
+                            </p>
+                            <p
+                                className={clsx(
+                                    scss.smear_text,
+                                    scss.desktop_text
+                                )}
+                            >
+                                Beauty ist unsere Leidenschaft. 2022 konnten wir
+                                nun endlich unser eigenes Studio im Herzen von
+                                Berlin eröffnen. Wir sind ein Zusammenschluss
+                                aus sechs Beauty-Experten. Die sich das Ziel
+                                gesetzt haben Menschen aus Berlin und aller Welt
+                                ihren Wunsch nach permanenter Schönheit zu
+                                erfüllen. Unser Leistungskatalog von ALLURE
+                                Permanent Make up & Beauty umfasst Permanent
+                                Make- up Behandlungen, Nageldesign,
+                                Wimpernverdichtung und Wimpernverlängerung. Neu
+                                in unserem Angebot sind zu dem Massagen.
                             </p>
                         </div>
                         <ReadMore />
