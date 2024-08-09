2ff233ebe2ddf5211b9e438dd3c97b6430800d3d
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-08-09 17:00:25 +0300

    added 404

--- src/assets/images/header/index.js
+++ src/assets/images/header/index.js
@@ -1,1 +1,5 @@
-export { default as logo } from './logo.png';
+export { default as logo_mob_1x } from './logo-mob.png';
+export { default as logo_mob_2x } from './logo-mob.png';
+
+export { default as logo_tab_1x } from './logo-tab.png';
+export { default as logo_tab_2x } from './logo-tab.png';
