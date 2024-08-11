a2ac5999cd20c095b24ba8a9a61beac5f9d068e9
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-08-11 15:34:26 +0200

    move pages to root page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import dynamic from 'next/dynamic';
+import Permanent from '@/components/Permanent/Permanent';
+import Hero from '@/components/Hero';
+import Feedbacks from '@/components/Feedbacks';
+import OurTeam from '@/components/OurTeam';
+import Location from '@/components/Location';
 
 const ServicesVideo = dynamic(() => import('@/components/Services'), {
     ssr: false,
@@ -10,7 +15,12 @@ const Home = () => {
     return (
         <main>
             <Header />
+            <Hero />
+            <Permanent />
             <ServicesVideo />
+            <Feedbacks />
+            <OurTeam />
+            <Location />
             <Footer />
         </main>
     );
