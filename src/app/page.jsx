30cb58d240fda808a3d6b508dd1ae74160a39e9d
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-08-11 15:20:37 +0200

    rename folder

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import HomePage from './(pages)/page';
+import HomePage from './(allure)/page';
 
 const Home = () => {
     return (
