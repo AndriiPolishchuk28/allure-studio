9d8da83a062ed37978fc8894144481af43250a35
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-10-05 19:10:15 +0200

    add pop_up iframe

--- src/components/BookingButton/BookingButton.jsx
+++ src/components/BookingButton/BookingButton.jsx
@@ -1,17 +1,42 @@
-import Link from 'next/link';
+'use client';
+
+// import Link from 'next/link';
 import scss from './BookingButton.module.scss';
+import { useState } from 'react';
 
 const BookingButton = () => {
+    const [showModal, setShowModal] = useState(false);
+    console.log(showModal);
+    const openModal = () => {
+        setShowModal(!showModal);
+    };
+
     return (
         <div>
-            <Link
+            {/* <Link
                 href="https://connect.shore.com/widget/allure-studio?locale=de"
                 className={scss.link}
                 target="_blank"
                 rel="noopener noreferrer"
             >
                 Jetzt Buchen
-            </Link>
+            </Link> */}
+            <button onClick={openModal} className={scss.link}>
+                Jetzt Buchen
+            </button>
+            {showModal && (
+                <div className={scss.wrapper_popup}>
+                    <iframe
+                        src="https://connect.shore.com/widget/allure-studio?locale=de"
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            border: 'none',
+                        }}
+                        title="Booking Widget"
+                    />
+                </div>
+            )}
         </div>
     );
 };
